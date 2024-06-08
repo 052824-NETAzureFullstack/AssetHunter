51ff34cb0c33876f08ed820b5cb0311e5f660f8d
Paris B <parisbg@Pariss-MBP-3.lan>
2024-06-08 18:58:46 -0500

    Created login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     //{path: 'login', component: LoginComponent,title: 'Login'},
     //{path: 'register', component: RegisterComponent, title: 'Register'},
-    {path: '', component: RegisterComponent, title: 'Asset Hunter'},
+    {path: '', component: LoginComponent, title: 'Asset Hunter'},
     //{path: 'results', component: ResultsComponent, Results'},
     //{path: 'strategies', component: StrategiesComponent, title: 'Strategies'},
     //{path: 'terms', component: TermsComponent, title: 'AssetHunter/Terms'}
