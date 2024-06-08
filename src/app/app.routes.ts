8a838b666054e1645e6cf9a861e15ded911a6ffd
garrettGray <42936770+ParisBG@users.noreply.github.com>
2024-06-08 11:16:20 -0500

    Decoupled app html into a component and created multiple components for later routing (#6)
    Co-authored-by: Paris B <parisbg@Pariss-MBP-3.lan>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { TermsComponent } from './terms/terms.component';
 
 export const routes: Routes = [
-    //{path: 'terms', component: PrivacyPolicyComponent}
+    //{path: 'login', component: LoginComponent,title: 'Login'},
+    //{path: 'register', component: RegisterComponent, title: 'Register'},
+    {path: '', component: MainComponent, title: 'Home Page'},
+    //{path: 'results', component: ResultsComponent, Results'},
+    //{path: 'strategies', component: StrategiesComponent, title: 'Strategies'},
+    {path: 'terms', component: TermsComponent, title: 'Terms and Conditions '}
+
+
 
 ];
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet,RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+  showError: boolean = false;
 
+  validateScore(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    let value = parseInt(input.value);
+    this.showError = (value < 300 || value > 850) ? true : false;
+  }
+
+  changeButtonColor(event: Event): void {
+    const button = event.target as HTMLInputElement;
+    button.classList.add('button-clicked');
+  }
 }
