2f9ef1e17f68343709b353e0806bb877345cf7ee
“ParisBG” <itzp32g@gmail.com>
2024-06-13 12:37:27 -0500

    Fixed form error and added FA icons internally

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import {NgForm} from '@angular/forms';
+import { FormsModule }   from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  onSubmit(f: NgForm) {
+    console.log(f.value); // { first: '', last: '' }
+    //console.log(f.valid); // false
+  }
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet,RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule,RouterOutlet, RouterModule],
+  imports: [CommonModule,RouterOutlet, RouterModule, FormsModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
