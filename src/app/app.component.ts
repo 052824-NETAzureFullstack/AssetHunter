94210df6545c1d0d4238576fb1a2eea5b3de22b4
“ParisBG” <itzp32g@gmail.com>
2024-07-02 18:53:00 -0500

    added hamburger menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterOutlet,RouterModule,Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet,RouterModule } from '@angular/router';
 
 
 @Component({
@@ -10,25 +10,40 @@ import { CommonModule } from '@angular/common';
   template: `
 
   <header class="header">
+
+  <nav>
+    <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+    <div class="nav-mobile">
+        <a href="#">Home</a>
+        <a href="#">?</a>
+        <a href="#">?</a>
+        <a href="#">?</a>
+    </div>
+  </nav>
+<!-- 
     <div class="hamburger" onclick="toggleMenu()">
       <span></span>
       <span></span>
       <span></span>
     </div>
+    --> 
     <a class="ah-logo-container" [routerLink]="['/']">
       <img class="ah-logo" src="/assets/assetHunterLogo3.png" alt="Asset Hunter Logo" aria-hidden="true">
     </a><br>
   </header>
 
     <main>
-      <section class="metrics">
+      <section>
         <router-outlet></router-outlet>
         <!-- only need one router outlet, have the outlet point to a component and have it wrap that decides how they are displayed... something that has a side-by-side strucure -->
       </section>
 
       <footer class="footer">
         <img class="gg-logo" src="assets/garrett_gray_LLC_logo.jpg" alt="Garrett Gray LLC logo" aria-hidden="true"><br><br><br><br><br><br>
-        <p>&copy; All rights reserved. Garrett Gray LLC&trade; 2025. </p>
+        <p>&copy; All rights reserved. Garrett Gray LLC&trade; .</p>
     </footer>
 
     </main>
@@ -38,5 +53,4 @@ import { CommonModule } from '@angular/common';
 
 export class AppComponent {
   title = 'Asset Hunter';
-
 }
